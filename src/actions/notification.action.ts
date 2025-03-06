5936c8fd61467dd205df228663b7ce9e0748527c
aassiimmaa <nguyenthanhanvp2690@gmail.com>
2025-03-06 17:05:38 +0700

    notifications page

---
+++ src/actions/notification.action.ts
@@ -0,0 +1,72 @@
+'use server'
+
+import React from 'react'
+import { getDbUserId } from './user.action'
+import prisma from '~/lib/prisma'
+
+const getNotifications = async () => {
+  try {
+    const userId = await getDbUserId()
+    if (!userId) return []
+
+    const notifications = await prisma.notification.findMany({
+      where: {
+        userId
+      },
+      include: {
+        creator: {
+          select: {
+            id: true,
+            name: true,
+            username: true,
+            image: true
+          }
+        },
+        post: {
+          select: {
+            id: true,
+            content: true,
+            image: true
+          }
+        },
+        comment: {
+          select: {
+            id: true,
+            content: true,
+            createdAt: true
+          }
+        }
+      },
+      orderBy: {
+        createdAt: 'desc'
+      }
+    })
+
+    return notifications
+  } catch (error) {
+    console.error('Error fetching notifications:', error)
+    return []
+  }
+}
+
+const markNotificationsAsRead = async(notificationIds: string[]) => {
+    try {
+      await prisma.notification.updateMany({
+        where: {
+          id: {
+            in: notificationIds,
+          },
+        },
+        data: {
+          read: true,
+        },
+      });
+  
+      return { success: true };
+    } catch (error) {
+      console.error("Error marking notifications as read:", error);
+      return { success: false };
+    }
+  }
+
+export { getNotifications, markNotificationsAsRead}
